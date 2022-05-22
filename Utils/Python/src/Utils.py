bb7a636b1fb6ceeb1e9fda4d0e9e50c328849576
Harvey <hjamesbarber@gmail.com>
2022-05-22 20:03:07 +0100

    Add functions to utils + tidy up code

--- Utils/Python/src/Utils.py
+++ Utils/Python/src/Utils.py
@@ -1,5 +1,39 @@
 import numpy as np
 
-def mag2dB(mag):
+
+def mag2db(mag):
+    """
+    Takes a linear amplitude and converts it to dB.
+
+    Parameters
+    ----------
+    mag : float
+        Linear magnitude
+
+    Returns
+    -------
+    float
+        The dB magnitude of the input value.
+
+    """
     
-    return 20*np.log10(np.abs(mag))
+    return 20 * np.log10(np.abs(mag))
+
+
+def db2mag(db):
+    """
+    Takes a dB magnitude and converts it to linear.
+
+    Parameters
+    ----------
+    db : float
+        dB magnitude.
+
+    Returns
+    -------
+    float
+        The linear magnitude of the input value.
+
+    """
+    
+    return 10**(db / 20.)
